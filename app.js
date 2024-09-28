6fed566aa95e425bfaf58b21e80756571a98cbe6
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-09-28 21:21:04 +0600

    done

---
+++ app.js
@@ -0,0 +1,6 @@
+// console.log("test");
+document.getElementById("submit").addEventListener("click", function () {
+  const inp = document.getElementById("inputNumber");
+  console.log(typeof inp.value);
+});
+// function
